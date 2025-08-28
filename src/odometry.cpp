0d698cd09de13f4605d4f49d37f003dd52aee756
sk1780-jpg <sk1780@srmist.edu.in>
2025-08-28 18:19:41 +0530

    Update odometry.cpp
    solution 4

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -1,32 +1,50 @@
 #include "odometry.h"
 #include <cmath>
-#include <ctime>
-#include <iterator>
-#include <numeric>
+#include <vector>
 
 using namespace std;
 
-Odometry::Odometry(double wheel_radius, double rpm)
-    : radius(wheel_radius), rpm(rpm) {
-  // Linear velocity (m/s) =(wheel circumference * revolutions per second)
-  double rps = rpm / 60.0;
-  linear_vel = 2 * M_PI * radius * rps;
-}
-
-double Odometry::distance(int x1, int y1, int x2, int y2) {
-  return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-}
-
-double Odometry::angle(int x1, int y1, int x2, int y2) {
-  // atan2 returns radians, convert to degrees
-  return atan2(y2 - y1, x2 - x1) * 180.0 / M_PI;
-}
-
 MotionCommand Odometry::computeCommands(vector<pair<int, int>> &path) {
+    MotionCommand res = {0.0, 0.0}; // total time (seconds), total angle (degrees)
 
-  MotionCommand res = {0.0, 0.0}; // store total time and angle traversed
+    if (path.size() < 2)
+        return res;  // no movement if path too short
 
- /* Implement you odometry logic here */ 
+    double total_time = 0.0;
+    double total_angle_change = 0.0;
 
-  return res;
+    // Compute initial heading angle from first segment
+    double prev_angle = angle(path[0].first, path[0].second, path[1].first, path[1].second);
+
+    for (size_t i = 1; i < path.size(); ++i) {
+        int x1 = path[i - 1].first;
+        int y1 = path[i - 1].second;
+        int x2 = path[i].first;
+        int y2 = path[i].second;
+
+        // Distance between two consecutive points
+        double dist = distance(x1, y1, x2, y2);
+        total_time += dist / linear_vel;  // time = distance / velocity
+
+        // Current segment heading
+        double current_angle = angle(x1, y1, x2, y2);
+
+        if (i > 1) {
+            // Calculate angle difference between previous and current segment
+            double angle_diff = current_angle - prev_angle;
+
+            // Normalize to [-180, 180]
+            while (angle_diff > 180) angle_diff -= 360;
+            while (angle_diff < -180) angle_diff += 360;
+
+            total_angle_change += fabs(angle_diff);
+        }
+
+        prev_angle = current_angle;
+    }
+
+    res.time_sec = total_time;
+    res.angle_deg = total_angle_change;
+
+    return res;
 }
